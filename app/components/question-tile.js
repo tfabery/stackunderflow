a981a98b5795c12387e646bce8efc65ad889b8b7
Tony Fabery <tfabery@gmail.com>
2016-08-26 16:39:34 -0700

    add favorite service to show favorited questions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favorites: Ember.inject.service(),
+
   numberOfAnswers: Ember.computed('question.answers', function() {
     return this.get('question.answers').content.length;
-  })
+  }),
+
+  actions: {
+    addToFavorite(question) {
+      this.get('favorites').add(question);
+    }
+  }
 });
