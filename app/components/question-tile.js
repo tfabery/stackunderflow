40c02ce13168e0dc27f80abc9e3847073743a413
Tony Fabery <tfabery@gmail.com>
2016-08-26 16:18:52 -0700

    add computed property of numberOfAnswers

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  fire: Ember.computed('question.answers', function() {
-    if (this.get('question.answers').content.length >= 3) {
-      return '<span class="glyphicon glyphicon-fire"></span>';
-    }
+  numberOfAnswers: Ember.computed('question.answers', function() {
+    return this.get('question.answers').content.length;
   })
 });
