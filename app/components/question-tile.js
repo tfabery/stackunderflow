826da3970f71062cf9832d43f067d06fa9856739
Tony Fabery <tfabery@gmail.com>
2016-08-26 15:52:40 -0700

    change numberOfAnswers to fire

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  numberOfAnswers: Ember.computed('question.answers', function() {
+  fire: Ember.computed('question.answers', function() {
     if (this.get('question.answers').content.length >= 3) {
       return "on Fire";
     }
