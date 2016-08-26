b020c24f99c7d4560ccd2716b5f859a0d1abedfd
Tony Fabery <tfabery@gmail.com>
2016-08-26 15:58:01 -0700

    fix fire glyphicon to show as an img and not in htm text

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   fire: Ember.computed('question.answers', function() {
     if (this.get('question.answers').content.length >= 3) {
-      return "on Fire";
+      return '<span class="glyphicon glyphicon-fire"></span>';
     }
   })
 });
